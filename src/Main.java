05726bdc86b2ea0c9ecd66fc00f8b03fb2b8759b
koalaa13 <nooshe981@gmail.com>
2021-10-28 01:53:41 +0300

    Exceptions are now ignored

--- src/Main.java
+++ src/Main.java
@@ -9,38 +9,40 @@ import java.util.Scanner;
 
 public class Main {
     public static void main(
-            String[] args) throws Exception {
-        System.out.println("Enter a expression in one line");
-        Scanner scanner = new Scanner(System.in);
-        String expression = scanner.nextLine();
-        Parser parser = new Parser();
+            String[] args) {
+        try {
+            System.out.println("Enter a expression in one line");
+            Scanner scanner = new Scanner(System.in);
+            String expression = scanner.nextLine();
+            Parser parser = new Parser();
 
-        System.out.println("Parsed tokens in infix form");
-        List<Token> infixTokens = parser.getTokens(expression);
-        infixTokens.forEach(token -> System.out.print(token.toString() + ' '));
-        System.out.println();
-        System.out.println();
+            List<Token> infixTokens = parser.getTokens(expression);
+            System.out.println("Parsed tokens in infix form");
+            infixTokens.forEach(token -> System.out.print(token.toString() + ' '));
+            System.out.println();
+            System.out.println();
 
+            ParserVisitor parserVisitor = new ParserVisitor();
+            for (Token token : infixTokens) {
+                token.accept(parserVisitor);
+            }
+            System.out.println("Parsed tokens in reversed polish notation");
+            List<Token> RPNTokens = parserVisitor.getReversedPolishNotation();
 
-        ParserVisitor parserVisitor = new ParserVisitor();
-        for (Token token : infixTokens) {
-            token.accept(parserVisitor);
+            PrinterVisitor printerVisitor = new PrinterVisitor(System.out);
+            for (Token token : RPNTokens) {
+                token.accept(printerVisitor);
+            }
+            System.out.println();
+            System.out.println();
+
+            CalcVisitor calcVisitor = new CalcVisitor();
+            for (Token token : RPNTokens) {
+                token.accept(calcVisitor);
+            }
+            System.out.println("Calculation result");
+            System.out.println(calcVisitor.getResult());
+        } catch (Exception ignored) {
         }
-        System.out.println("Parsed tokens in reversed polish notation");
-        List<Token> RPNTokens = parserVisitor.getReversedPolishNotation();
-
-        PrinterVisitor printerVisitor = new PrinterVisitor(System.out);
-        for (Token token : RPNTokens) {
-            token.accept(printerVisitor);
-        }
-        System.out.println();
-        System.out.println();
-
-        CalcVisitor calcVisitor = new CalcVisitor();
-        for (Token token : RPNTokens) {
-            token.accept(calcVisitor);
-        }
-        System.out.println("Calculation result");
-        System.out.println(calcVisitor.getResult());
     }
 }
